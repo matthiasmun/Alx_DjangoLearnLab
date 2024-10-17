010a5908e7fb01e3c4c84771f35b426fa8f0bee7
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 20:29:39 +0200

    DD

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -51,3 +51,15 @@ urlpatterns = [
     # Other paths...
 ]
 
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    # Other paths...
+]
+
+
