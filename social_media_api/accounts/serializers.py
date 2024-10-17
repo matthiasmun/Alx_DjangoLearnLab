d91136ba0f40787ee2ac76bfb899b0cb92a4644a
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 21:14:22 +0200

    D

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -17,3 +17,37 @@ class UserSerializer(serializers.ModelSerializer):
             profile_picture=validated_data.get('profile_picture', None),
         )
         return user
+
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)  # Create a token for the user
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs['username'], password=attrs['password'])
+        if user is None:
+            raise serializers.ValidationError('Invalid credentials')
+        
+        return attrs
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,3 +4,12 @@ from .views import RegisterUser
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
 ]
+
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    # Add more URLs for profile management as needed
+]
