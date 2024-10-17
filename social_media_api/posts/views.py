c1f502fb8a36fefca8c5d630a060c6d4dcce3e5b
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 21:45:07 +0200

    DD

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from .serializers import UserSerializer
+
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly  # Custom permission
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)  # Automatically set the author to the logged-in user
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)  # Automatically set the author to the logged-in user
