8b0cc92adfa97d37079ea8f21c7b46bc2ad00e81
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 21:47:37 +0200

    D

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -28,4 +28,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/posts/', include('posts.urls')),  # Include URLs for posts
+    path('api/accounts/', include('accounts.urls')),  # Include URLs for accounts
 ]
+
+]
+ posts.urls api/
