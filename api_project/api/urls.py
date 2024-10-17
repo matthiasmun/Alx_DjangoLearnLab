7bbec74aa49015bcb89312642e7bc6c1f479fd4d
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 18:25:59 +0200

    EW

---
+++ api_project/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/', include('api.urls')),
+]
