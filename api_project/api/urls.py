fbf131f46305e56a803f4f986a17782bf77d3417
matthiasmun <matthiasmaxim123@gmail.com>
2024-10-17 18:29:52 +0200

    DWD

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from .views import BookList
+from django.contrib import admin
+from django.urls import path, include  # Add 'include'
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
-
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # This line includes the URLs from the 'api' app
 ]
